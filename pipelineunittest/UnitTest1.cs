62fcca30afced14d4e0c4fd7cbecb43421bc8236
rahulrai <rahulrai@deloitte.com>
2023-01-03 19:19:35 +0530

    added test

---
+++ pipelineunittest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace pipelineunittest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var result = false;
+        if(1+1 == 2)
+        {
+            result = true;
+        }
+        Assert.IsTrue(result, "test is passed");
+    }
+}
